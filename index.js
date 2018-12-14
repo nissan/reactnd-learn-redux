06c3807be5c5d7bf38693a90325f9ac9a3b1b8ce
Nissan Dookeran <nissan.dookeran@gmail.com>
2018-12-15 00:21:19 +1000

    show how to dispatch ADD_TODO action and update state

--- index.js
+++ index.js
@@ -7,34 +7,47 @@ function createStore(reducer) {
   // 4. Update the state
 
   let state;
-  let listeners = []
+  let listeners = [];
 
   const getState = () => state;
 
-  const subscribe = (listener) => {
+  const subscribe = listener => {
     listeners.push(listener);
     return () => {
-      listeners = listeners.filter((l)=> l !== listener)
-    }
-  }
+      listeners = listeners.filter(l => l !== listener);
+    };
+  };
 
-  const dispatch = (action) => {
+  const dispatch = action => {
     state = reducer(state, action);
-    listeners.forEach((listerer) => listener());
-  }
+    listeners.forEach(listener => listener());
+  };
   return {
     getState,
-    subscribe
+    subscribe,
+    dispatch
   };
 }
 
 // App Code
-function todos (state = [], action) {
-  if (action.type==='ADD_TODO') {
+function todos(state = [], action) {
+  if (action.type === "ADD_TODO") {
     return state.concat([action.todo]);
   }
 
   return state;
 }
 
-// const store = createStore(todos);
+const store = createStore(todos);
+store.subscribe(() => {
+  console.log("The new state is: ", store.getState());
+});
+
+store.dispatch({
+  type: "ADD_TODO",
+  todo: {
+    id: 0,
+    name: "Learn Redux",
+    complete: false
+  }
+});
