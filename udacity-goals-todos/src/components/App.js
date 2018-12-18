45d8cddd5dee28339125cc9d2ca6f8674fcdde9b
Nissan Dookeran <nissan.dookeran@gmail.com>
2018-12-18 20:58:59 +1000

    bring across components into individual files with correct references'

--- udacity-goals-todos/src/components/App.js
+++ udacity-goals-todos/src/components/App.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
+import ConnectedGoals from "./Goals";
+import ConnectedTodos from "./Todos";
 
-
-class App extends Component {
+class App extends React.Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(handleInitialData());
+  }
   render() {
+    const { loading } = this.props;
+    if (loading === true) {
+      return <h3>Loading...</h3>;
+    }
+
     return (
       <div>
-        Hello world
+        <ConnectedTodos />
+        <ConnectedGoals />
       </div>
     );
   }
 }
 
-export default App;
+export default connect(state => ({
+  loading: state.loading
+}))(App);
