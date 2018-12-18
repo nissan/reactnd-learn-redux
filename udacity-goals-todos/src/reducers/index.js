f0249dc3de12f4b4a2d4fe38e33bfed0277fd5d7
Nissan Dookeran <nissan.dookeran@gmail.com>
2018-12-18 20:44:25 +1000

    move reducers into site structure and individual files

---
+++ udacity-goals-todos/src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import goals from "./goals";
+import loading from "./loading";
+import todos from "./todos";
+
+export default combineReducers({
+  todos,
+  goals,
+  loading
+});
